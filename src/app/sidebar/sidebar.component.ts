d5c7411a1885ec27dc8353ed92dd043371860c71
tacettinsertkaya <tacettinsertkaya@outlook.com>
2020-11-21 02:36:48 +0300

    online edited

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -6,7 +6,10 @@ import {
   AfterContentInit,
 } from "@angular/core";
 import { Router } from "@angular/router";
+import { User } from "app/models/user";
 import { UserService } from "app/services/user.service";
+import { first } from "rxjs/operators";
+declare var $: any;
 
 //Metadata
 export interface RouteInfo {
@@ -154,6 +157,7 @@ export const ROUTES: RouteInfo[] = [
 export class SidebarComponent {
   constructor(private router: Router, private authService: UserService) { }
   public menuItems: any[];
+  users: Array<User> = [];
   isNotMobileMenu() {
     if (window.outerWidth > 991) {
       return false;
@@ -172,9 +176,22 @@ export class SidebarComponent {
 
   }
 
+  isLeader() {
+  
+    return this.authService.hasRole("Leader");
+
+  }
+
   ngOnInit() {
+   
+
+    if(this.isLeader() || this.isAdmin() ){
+      this.getAllUser();
+    }
+
     if(this.isAdmin()){
       this.menuItems = ROUTES.filter((menuItem) => menuItem);
+      
     }
    else{
     this.menuItems=[{
@@ -187,4 +204,39 @@ export class SidebarComponent {
    }
   }
   ngAfterViewInit() {}
+
+
+  
+  getAllUser() {
+    let filterRoles=["Member"];
+
+    
+    this.authService
+      .getAllUser(filterRoles)
+      .pipe(first())
+      .subscribe(
+        (res) => {
+          this.users = res;
+        },
+        (error) => {
+          $.notify(
+            {
+              icon: "ti-gift",
+              message: "İşlem sırasında hata oluştu.",
+            },
+            {
+              type: "danger",
+              timer: 4000,
+              placement: {
+                from: "top",
+                align: "right",
+              },
+              template:
+                '<div data-notify="container" class="col-11 col-md-4 alert alert-{0} alert-with-icon" role="alert"><button type="button" aria-hidden="true" class="close" data-notify="dismiss"><i class="nc-icon nc-simple-remove"></i></button><span data-notify="icon" class="nc-icon nc-bell-55"></span> <span data-notify="title">{1}</span> <span data-notify="message">{2}</span><div class="progress" data-notify="progressbar"><div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div></div><a href="{3}" target="{4}" data-notify="url"></a></div>',
+            }
+          );
+        }
+      );
+  }
+
 }
